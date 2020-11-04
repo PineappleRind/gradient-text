666cc827e312a7d5d30f942f089e4bb50a9a9fcc
northernlights03 <65303441+northernlights03@users.noreply.github.com>
2020-11-03 20:34:48 -0500

    Stop modal added

--- index.js
+++ index.js
@@ -6,10 +6,34 @@ function textFunction() {
     let text = document.getElementById('output');
 
     let inputText = document.getElementById('textContentInput');
+    let inputBeginningColor = document.getElementById('gradientBeginning');
+    let inputEndColor = document.getElementById('gradientEnd');
 
+    text.style.backgroundImage = 'linear-gradient(' + inputBeginningColor.value + ', ' + inputEndColor.value + ');'
     text.innerHTML = inputText.value;
 }
 
-body.addEventListener("input", function(){
+function editStop() {
+    let stopModal = document.getElementById('stopModal');
+    let overlay = document.getElementById('overlay');
+    if (overlay.style.opacity !== '1'){
+    overlay.style.display = 'block';
+    overlay.style.opacity = '1';
+    stopModal.style.animationName = "scale-in";
+    stopModal.style.display = 'block';
+    } else {
+        overlay.style.opacity = '0';
+        stopModal.style.animationName = "scale-out";
+        setTimeout(function() {
+            stopModal.style.display = 'none';
+            overlay.style.display = 'none';
+        },1000)
+    }
+
+    setTimeout(function() {
+        stopModal.style.animationName = '';
+    },1000)
+}
+document.addEventListener("input", function(){
     textFunction();
 })
