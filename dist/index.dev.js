cf80b46cb5317edd6b70a2dd9aad2bff517b6d6d
northernlights03 <65303441+northernlights03@users.noreply.github.com>
2020-11-09 16:47:39 -0500

    Major changes: see description
    ADDITIONS
    Add stop button
    Stop sorting algorithm
    
    FIXES
    Stop sorting algorithm was bad, fixed

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -6,32 +6,54 @@ Reproduce with permission only */
 function textFunction() {
   var text = document.getElementById('output');
   var inputText = document.getElementById('textContentInput');
-  var inputBeginningColor = document.getElementById('gradientBeginning');
-  var inputEndColor = document.getElementById('gradientEnd');
-  text.style.backgroundImage = 'linear-gradient(to left,' + inputBeginningColor.value + ',' + inputEndColor.value + ');';
   text.innerHTML = inputText.value;
 }
 
 function editStop() {
   var stopModal = document.getElementById('stopModal');
   var overlay = document.getElementById('overlay');
+  overlay.style.display = 'block';
+  overlay.style.opacity = '1';
+  stopModal.style.animationName = "scale-in";
+  stopModal.style.display = 'block';
+  setTimeout(function () {
+    stopModal.style.animationName = '';
+  }, 800);
+}
+
+function addStopModalOpen() {
+  var addStopModal = document.getElementById('addStopModal');
+  var overlay = document.getElementById('overlay');
 
   if (overlay.style.opacity !== '1') {
     overlay.style.display = 'block';
     overlay.style.opacity = '1';
-    stopModal.style.animationName = "scale-in";
-    stopModal.style.display = 'block';
-  } else {
-    overlay.style.opacity = '0';
-    stopModal.style.animationName = "scale-out";
-    setTimeout(function () {
-      stopModal.style.display = 'none';
-      overlay.style.display = 'none';
-    }, 800);
+    addStopModal.style.animationName = "scale-in";
+    addStopModal.style.display = 'block';
   }
 
   setTimeout(function () {
-    stopModal.style.animationName = '';
+    addStopModal.style.animationName = '';
+  }, 800);
+}
+
+function addStop() {
+  var stopPos = document.getElementById('addStopPosition');
+  document.getElementById("gradientStop").insertAdjacentHTML('afterend', "<li onclick=\"editStop();\" id=\"gradientStop\" class=\"gradient-stop\">".concat(stopPos.value, "<div class=\"top\"><h3>Click to edit</h3><i></i></div></li>"));
+  var list = document.getElementById('stopList');
+  tinysort('ul#xnum>li', {
+    natural: true
+  });
+}
+
+function closeModal() {
+  overlay.style.opacity = '0';
+  addStopModal.style.animationName = "scale-out";
+  stopModal.style.animationName = "scale-out";
+  setTimeout(function () {
+    addStopModal.style.display = 'none';
+    overlay.style.display = 'none';
+    stopModal.style.display = 'none';
   }, 800);
 }
 
--- index.js
+++ index.js
@@ -6,33 +6,54 @@ function textFunction() {
     let text = document.getElementById('output');
 
     let inputText = document.getElementById('textContentInput');
-    let inputBeginningColor = document.getElementById('gradientBeginning');
-    let inputEndColor = document.getElementById('gradientEnd');
-
-    text.style.backgroundImage = 'linear-gradient(to left,' + inputBeginningColor.value + ',' + inputEndColor.value + ');'
     text.innerHTML = inputText.value;
 }
 
 function editStop() {
     let stopModal = document.getElementById('stopModal');
     let overlay = document.getElementById('overlay');
-    if (overlay.style.opacity !== '1') {
         overlay.style.display = 'block';
         overlay.style.opacity = '1';
         stopModal.style.animationName = "scale-in";
         stopModal.style.display = 'block';
-    } else {
-        overlay.style.opacity = '0';
-        stopModal.style.animationName = "scale-out";
-        setTimeout(function() {
-            stopModal.style.display = 'none';
-            overlay.style.display = 'none';
-        }, 800)
-    }
-
     setTimeout(function() {
         stopModal.style.animationName = '';
     }, 800)
+}
+
+function addStopModalOpen() { 
+    let addStopModal = document.getElementById('addStopModal');
+    let overlay = document.getElementById('overlay');
+    if (overlay.style.opacity !== '1') {
+        overlay.style.display = 'block';
+        overlay.style.opacity = '1';
+        addStopModal.style.animationName = "scale-in";
+        addStopModal.style.display = 'block';
+    }
+
+    setTimeout(function() {
+        addStopModal.style.animationName = '';
+    }, 800);
+
+}
+
+function addStop() {
+    let stopPos = document.getElementById('addStopPosition');
+    document.getElementById("gradientStop").insertAdjacentHTML('afterend', 
+            `<li onclick="editStop();" id="gradientStop" class="gradient-stop">${stopPos.value}<div class="top"><h3>Click to edit</h3><i></i></div></li>`);  
+            let list = document.getElementById('stopList')
+            tinysort('ul#xnum>li',{natural:true})
+}
+
+function closeModal() {
+    overlay.style.opacity = '0';
+        addStopModal.style.animationName = "scale-out";
+        stopModal.style.animationName = "scale-out";
+        setTimeout(function() {
+            addStopModal.style.display = 'none';
+            overlay.style.display = 'none';
+            stopModal.style.display = 'none';
+        }, 800)
 }
 document.addEventListener("input", function() {
     textFunction();
